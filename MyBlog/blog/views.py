9fc40accc04476236f38c43be5ae067d4e9b5b90
AleksandarBuk <aleksandar.buk@gmail.com>
2023-06-19 21:46:00 +0200

    Second commit
    Added templates, functionality and database

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+posts = [
+	{
+		'authors': 'AleksandarBukvic',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'August 27,2018'
+	},
+
+	{
+		'authors':'Jane Dough',
+		'title': 'Blog Post 2',
+		'content':'Second Blog Content',
+		'date_posted': 'August 28, 2019'
+	}
+]
 
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {
+		'posts': Post.objects.all()
+	}
+	return render(request, 'blog/home.html', context)
 # Create your views here.
-
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html', {'title': 'About'})
