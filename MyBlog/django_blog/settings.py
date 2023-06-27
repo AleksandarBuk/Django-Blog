dd66b81e78d02c63a6b6ede568f4a28fa021f3d8
AleksandarBuk <aleksandar.buk@gmail.com>
2023-06-27 22:51:04 +0200

    added password reset functinoality

--- MyBlog/django_blog/settings.py
+++ MyBlog/django_blog/settings.py
@@ -135,3 +135,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gamil.com'
+EMAIL_PORT = 587
+EMAIl_USE_TLS = True
+EMAIL_HOST_USER = 'aleksandar.buk@gmail.com'
+EMAIL_HOST_PASSWORD = 'scbkcycwswkdqohy'
