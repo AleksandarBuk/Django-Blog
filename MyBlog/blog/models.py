c880c2a430bb8a593b7eebd1c5ad845d300a1f33
Aleksandar Bukvic <aleksandar.buk@gmail.com>
2023-11-10 23:38:04 +0100

    Updated models/form/views

--- MyBlog/blog/models.py
+++ MyBlog/blog/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from tinymce.models import HTMLField  # Import HTMLField
+
 
 class Post(models.Model):
-	title = models.CharField(max_length=100)
-	content = models.TextField()
-	date_posted = models.DateTimeField(default=timezone.now)
-	author_id = models.ForeignKey(User, on_delete=models.CASCADE) 
+    title = models.CharField(max_length=100)
+    content = HTMLField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-	def get_absolute_url(self):
-		return reverse('post-detail', kwargs={'pk': self.pk})
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- MyBlog/users/views.py
+++ MyBlog/users/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
 from django.contrib.auth.decorators import login_required
 from .form import (
-	UserCreationForm, 
-	UserUpdateForm, 
-	ProfileUpdateForm, 
-	UserRegisterForm,
-	)
+    UserCreationForm,
+    UserUpdateForm,
+    ProfileUpdateForm,
+    UserRegisterForm,
+)
+
 
 def register(request):
     if request.method == 'POST':
@@ -19,11 +21,13 @@ def register(request):
 
     return render(request, 'user/register.html', {'form': form})
 
+
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -32,10 +36,9 @@ def profile(request):
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-        
+
     context = {
         'u_form': u_form,
         'p_form': p_form
     }
     return render(request, 'user/profile.html', context)
-
